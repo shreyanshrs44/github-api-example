e1e6dae7269e9291ede3ea47ba677e06d18b656c
shreyanshrs44 <shreyanshrs44@gmail.com>
2020-07-14 12:35:08 +0530

    Added functionality for creating and updating files in the repository

--- src/main/java/com/shreyanshrs44/githubapiexample/controller/ApiController.java
+++ src/main/java/com/shreyanshrs44/githubapiexample/controller/ApiController.java
@@ -10,11 +10,10 @@ import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
 import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.net.URL;
 
 @RestController
 public class ApiController {
@@ -49,5 +48,36 @@ public class ApiController {
         myRepository.create();
         return repoName;
     }
-    
+
+    @GetMapping("/createFile/{repoName}/{fileName}")
+    public URL createFile(@PathVariable(name="repoName") String repoName,@PathVariable(name="fileName") String fileName) throws IOException {
+        GitHub github = new GitHubBuilder().withAppInstallationToken(githubUser.getAccessToken()).build();
+        GHUser user = github.getMyself();
+        String username = user.getLogin();
+        GHRepository myRepo = github.getRepository(username+"/"+repoName);
+        GHContentBuilder contentBuilder = myRepo.createContent();
+        contentBuilder.content("class "+fileName+"{ }");
+        contentBuilder.path(fileName+".java");
+        contentBuilder.message("Creating File");
+        contentBuilder.branch("master");
+        GHContentUpdateResponse response = contentBuilder.commit();
+        return response.getCommit().getHtmlUrl();
+    }
+
+    @GetMapping("/updateFile/{repoName}/{fileName}/{fileContent}")
+    public URL updateFile(@PathVariable(name="repoName") String repoName,@PathVariable(name="fileName") String fileName,@PathVariable(name="fileContent") String fileContent) throws IOException {
+        GitHub github = new GitHubBuilder().withAppInstallationToken(githubUser.getAccessToken()).build();
+        GHUser user = github.getMyself();
+        String username = user.getLogin();
+        GHRepository myRepo = github.getRepository(username+"/"+repoName);
+        GHContent content = myRepo.getFileContent(fileName);
+        GHContentBuilder contentBuilder = myRepo.createContent();
+        contentBuilder.sha(content.getSha());
+        contentBuilder.branch("master");
+        contentBuilder.path(fileName);
+        contentBuilder.content(fileContent);
+        contentBuilder.message("Updating File");
+        GHContentUpdateResponse response = contentBuilder.commit();
+        return response.getCommit().getHtmlUrl();
+    }
 }
